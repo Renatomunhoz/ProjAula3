f9bb35bfb3a03076707f2188b9d5df1c641e6f9a
Aluno Fiar Logatti <aluno@fiar.local>
2018-02-28 21:24:58 -0300

    atualização 21:24

--- src/br/com/proj/dados/MotoDB.java
+++ src/br/com/proj/dados/MotoDB.java
@@ -1,14 +1,24 @@
 package br.com.proj.dados;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.List;
 
 import br.com.proj.entidade.Moto;
+import br.com.proj.util.Conexao;
 
 public class MotoDB implements IMotoDB{
 
+	private Connection con;
+	private PreparedStatement ps;
+	
+	
 	public MotoDB() {
 		
 		// ABERTURA DA CONEXÃO COM A BASE DE DADOS 
+		
+		con = Conexao.getConnectio();
 		
 	}
 	
@@ -21,8 +31,29 @@ public class MotoDB implements IMotoDB{
 		
 	//IMPLEMENTAÇÃO DO INSERT NA BASE DE DADOS 	
 		
-		return true;
-		
+     boolean status = false;
+     
+     StringBuilder sb = new StringBuilder();
+     
+     sb.append("INSERT INTO MOTO");
+     sb.append("(id, descricao,ano) values (?,?,?)");
+     
+     try {
+		ps = this.con.prepareStatement(sb.toString());
+	 
+     
+     ps.setInt (1, moto.getId());
+     ps.setString(2, moto.getDescricao());
+     ps.setInt(3, moto.getAno());
+     
+     return ps.execute();
+	
+     }catch (SQLException e) {
+ 		// TODO Auto-generated catch block
+ 		e.printStackTrace();
+ 	}
+     
+     return false;
 	}
 
 
